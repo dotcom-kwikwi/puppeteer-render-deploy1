43544f2c5fe9fdb369b84d884eba44f2bbdbe79b
jasmin1993 <jasminnajjaci@gmail.com>
2025-02-17 04:34:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -36,6 +36,9 @@ app.get("/scrape", async (req, res) => {
             });
 
             const page = await browser.newPage();
+
+            await page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36");
+
             
             page.on('response', async (response) => {
                 console.log(`📡 AJAX Response: ${response.url()}`);
