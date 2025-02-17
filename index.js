9fb9648a64c22b6a16e9d8867ed3a341256279b1
jasmin1993 <jasminnajjaci@gmail.com>
2025-02-17 02:00:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -39,16 +39,6 @@ app.get("/scrape", async (req, res) => {
             await page.goto("https://www.kupujemprodajem.com/bela-tehnika-i-kucni-aparati/ves-masine/pretraga?categoryId=15&groupId=188&locationId=1&priceTo=150&currency=eur&order=posted%20desc");
             const title = await page.title();
 
-            //await page.waitForSelector('.AdItem_adOuterHolder__lACeh');
-            //const ads = await page.evaluate(() => {
-                //return Array.from(document.querySelectorAll('.AdItem_adOuterHolder__lACeh'))
-                    //.map(ad => ad.id.match(/\d+/)?.[0])
-                    //.filter(Boolean);
-            //});
-
-            //console.log(`🔍 Pronađeno ${ads.length} oglasa.`);
-            
-
             console.log("Puppeteer started successfully!");
             res.send({title});
         } catch (error) {
